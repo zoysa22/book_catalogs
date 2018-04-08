3beedb483ad82417d480d9224f14782e68ef1dca
zoysa22 <37470092+zoysa22@users.noreply.github.com>
2018-04-08 20:25:42 +0100

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+# app/__init__.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.login_view = 'authentication.do_the_login'
+login_manager.session_protection = 'strong'
+bcrypt = Bcrypt()
+
+
+def create_app(config_type):  # dev , test , prod
+    app = Flask(__name__)
+
+    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
+    app.config.from_pyfile(configuration)
+
+    db.init_app(app)
+    bootstrap.init_app(app)  # initialize bootstrap
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+
+    from app.catalog import main  # import blueprint
+    app.register_blueprint(main)
+
+    from app.auth import authentication
+    app.register_blueprint(authentication)
+
+    return app
